a60bae383e02ab8aad73561b5e9f545a0fd4be9d
potter <potter@cmus.com>
2014-10-10 15:38:09 +0800

    setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@
 from setuptools import setup, find_packages
 
 setup (
-    name = 'SimpleHTTP Server - Client',
+    name = 'SimpleHTTPServerClient',
     version = '1.0',
     description = 'SimpleHTTP Server - Client',
     url = 'http://dogeplan.com',
     license = 'LGPL',
     packages = find_packages(),
-    script = ['client/SimpleHTTPClient.py', 'server/SimpleHTTPServer.py'],
-    extras_require = {
-        'requests': ['requests'],  
-    },
+    scripts = ['client/SimpleHTTPClient.py', 'server/SimpleHTTPServer.py'],
+    #extras_require = {
+    #    'requests': ['requests'],  
+    #},
     )
