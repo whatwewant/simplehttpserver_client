75b15e67d3f060ca3f6bddfecafef371b7c68589
potter <potter@cmus.com>
2014-10-10 16:47:27 +0800

    ...

--- server/SimpleHTTPServer.py
+++ server/SimpleHTTPServer.py
@@ -5,10 +5,14 @@ import platform
 PYTHON_VERSION = platform.python_version()
 import os
 COMMAND = '{python_version} {server} {argv}'
+file_path = os.path.dirname(__file__)
 import sys
+argvs = ''
+for argv in sys.argv:
+    argvs += (argv + ' ')
 
 if PYTHON_VERSION.startswith('3'):
-    os.system(COMMAND.format(python_version='python', server='server_python3.py', argv=sys.argv[1]))
+    os.system(COMMAND.format(python_version='python', server=file_path+'/server_python3.py', argv=argvs))
 
 else:
-    os.system(COMMAND.format(python_version='python', server='server_python2.py', argv=sys.argv[1]))
+    os.system(COMMAND.format(python_version='python', server=file_path+'/server_python2.py', argv=argvs))
