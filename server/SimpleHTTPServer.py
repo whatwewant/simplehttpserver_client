ff0672ca4debf729f491a58326b8c041c32f6e96
potter <potter@cmus.com>
2014-10-11 17:17:16 +0800

    + server: --prefix=

--- server/SimpleHTTPServer.py
+++ server/SimpleHTTPServer.py
@@ -2,18 +2,46 @@
 # coding=utf-8
 
 import platform
-PYTHON_VERSION = platform.python_version()
-import os
-COMMAND = '{python_version} {server} {argv}'
-file_path = os.path.dirname(__file__)
 import sys
-argvs = ''
-for argv in sys.argv:
-    if argv != sys.argv[0]:
-        argvs += (argv + ' ')
+import os
 
-if PYTHON_VERSION.startswith('3'):
-    os.system(COMMAND.format(python_version='python', server=file_path+'/server_python3.py', argv=argvs))
+PYTHON_VERSION = platform.python_version()
+COMMAND = '{python_version} -m {server} {argv}'
+file_path = os.path.dirname(__file__)
 
-else:
-    os.system(COMMAND.format(python_version='python', server=file_path+'/server_python2.py', argv=argvs))
+# SYS PATH
+#sys.path.append(os.path.join(file_path, '..'))
+#from server.server_python2 import test
+
+def usage(command):
+    print('Usage:')
+    print('\t%s Port(Default 8000)' % command)
+    print('\t%s Port' % command)
+    print('\t%s --prefix=path/to/dir' % command)
+    print('\t%s --prefix=path/to/dir' % command)
+    print('\t%s Port --prefix=path/to/dir\n' % command)
+
+if __name__ == '__main__':
+    '''main'''
+    usage(sys.argv[0])
+    if len(sys.argv) > 3:
+        exit(-1)
+
+    argvs = ''
+    for argv in sys.argv:
+        if argv != sys.argv[0] and '--prefix=' not in argv:
+            argvs += (argv + ' ')
+
+        if '--prefix=' in argv:
+            chdir = argv.strip().replace('--prefix=', '')
+            chdir = chdir.replace('~', os.environ.get('HOME'))
+            os.chdir(chdir)
+
+    # Current Dir
+    print('Current Directory: %s\n' % os.getcwd())
+
+    if PYTHON_VERSION.startswith('3'):
+        os.system(COMMAND.format(python_version='python', server='http.server', argv=argvs))
+
+    else:
+        os.system(COMMAND.format(python_version='python', server='SimpleHTTPServer', argv=argvs))
