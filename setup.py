566e61e7d42a86e2070afc1dce9821948bccdcb5
potter <potter@cmus.com>
2014-10-12 10:26:51 +0800

    SimpleHTTPServerClient 2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup (
     name = 'SimpleHTTPServerClient',
-    version = '1.0',
+    version = '2.0',
     description = 'SimpleHTTP Server - Client',
     url = 'http://dogeplan.com',
     license = 'LGPL',
